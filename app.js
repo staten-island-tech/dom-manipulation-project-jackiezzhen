fb76dfa3376c9a74819dc70e981c028d4f9c1fd4
jackiezzhen <jackiezhen31@gmail.com>
2023-10-17 23:03:26 -0400

    s

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 /*const form = document.querySelector("#form");
 console.log(form); */
-const DOMSelectors= {
-    form: document.querySelector("#form"),
-    firstName: document.querySelector(".firstName"),
-    //grab the text input
-    //grab ALL the h2s
-    h2: document.querySelectorAll("h2"),
+const DOMSelectors = {
+  form: document.querySelector("#form"),
+  firstName: document.querySelector(".firstName"),
+
+  h2: document.querySelectorAll("h2"), //grab the text input, grab ALL the h2s
 };
 //console.log(DOMSelectors.h2);
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
-    DOMSelectors.h2.forEach(
-        (e1) => e1.textContent = DOMSelectors.firstName.value);
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(DOMSelectors.firstName.value);
+  DOMSelectors.h2.forEach(
+    (h2) => (h2.textContent = DOMSelectors.firstName.value)
+  );
 });
