383568711b27ebac0928847acad7ead15bdcd99f
jackiezzhen <jackiezhen31@gmail.com>
2023-10-24 09:25:27 -0400

    center

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-/*const form = document.querySelector("#form");
-console.log(form); */
 const DOMSelectors= {
     form: document.querySelector("#form"),
     name: document.querySelector(".name"),
-    //grab the text input
-    //grab ALL the h2s
+    type: document.querySelector(".type"),
+    move: document.querySelector(".move"),
     h2: document.querySelectorAll("h2"),
 };
-//console.log(DOMSelectors.h2);
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.name.value);
