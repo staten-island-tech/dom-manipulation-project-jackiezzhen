0429e07f6b9e7c0e0c798542cb4efbf966c9796a
jackiezzhen <jackiezhen31@gmail.com>
2023-10-25 21:43:22 -0400

    css
    move form to left, create cards on the right side, fix overlap

--- app.js
+++ app.js
@@ -1,19 +1,59 @@
-const DOMSelectors= {
-    form: document.querySelector("#form"),
-    name: document.querySelector(".name"),
-    type: document.querySelector(".type"),
-    move: document.querySelector(".move"),
-    h2: document.querySelectorAll("h2"),
+const DOMSelectors = {    
+    form: document.querySelector(".form"),
+    image: document.getElementById("image"),
+    name: document.getElementById("name"),
+    type: document.getElementById("type"),
+    move: document.getElementById("move"),
+    container: document.querySelector(".card"),
 };
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
+
+function createObject() {
+  const nameValue = DOMSelectors.name.value;
+  const typeValue = DOMSelectors.type.value;
+  const moveValue = DOMSelectors.move.value;
+  const imageurl = DOMSelectors.image.value;
+
+  return {
+    name: nameValue,
+    type: typeValue,
+    move: moveValue,
+    imageurl: imageurl,
+  };
+}
+
+function injectObject(object) {
+  const cardHTML = `<div class="card">
+    <img src="${object.imageUrl}" alt="Image/Error">
+    <p>Name: ${object.name}</p>
+    <p>Type: ${object.type}</p>
+    <p>Moves: ${object.move}</p>
+
+    <button class="remove">Remove</button>
+    </div>`;
+
+  DOMSelectors.container.insertAdjacentHTML("beforeend", cardHTML);
+}
+
+function ClearInputFields() {
+  nameValue.value = "";
+  typeValue.value = "";
+  moveValue.value = "";
+  imageurl.value = "";
+}
+/*
+function removeObject(event) {
+  }
+*/
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const newobject = createObject();
+  injectObject(newobject);
+  ClearInputFields();
+});
+
+/*
     console.log(DOMSelectors.name.value);
     DOMSelectors.h2.forEach(
         (e1) => e1.textContent = DOMSelectors.name.value);
-    DOMSelectors.name.value.insertAdjacentHTML(
-        'afterend',
-        "<strong>inserted text</strong>",
-          );
 });
-/*
-function copycard(copy){}*/
+*/
