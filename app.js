ef280b3d2b3ebb7622be58c26714663a8e8cf766
jackiezzhen <jackiezhen31@gmail.com>
2023-10-26 23:54:34 -0400

    mdae remove

--- app.js
+++ app.js
@@ -1,56 +1,68 @@
-const DOMSelectors = {    
-    form: document.querySelector(".form"),
-    image: document.getElementById("image"),
-    name: document.getElementById("name"),
-    type: document.getElementById("type"),
-    move: document.getElementById("move"),
-    container: document.querySelector(".card"),
+const DOMSelectors = {
+  form: document.querySelector(".form"),
+  image: document.getElementById("image"),
+  name: document.getElementById("name"),
+  type: document.getElementById("type"),
+  move1: document.getElementById("move1"),
+  move2: document.getElementById("move2"),
+  container: document.querySelector(".card"),
 };
 
-function createObject() {
+function createobject() {
   const nameValue = DOMSelectors.name.value;
   const typeValue = DOMSelectors.type.value;
-  const moveValue = DOMSelectors.move.value;
+  const move1Value = DOMSelectors.move1.value;
+  const move2Value = DOMSelectors.move2.value;
   const imageurl = DOMSelectors.image.value;
 
   return {
-    names: nameValue,
-    types: typeValue,
-    moves: moveValue,
-    imageurls: imageurl,
+    name: nameValue,
+    type: typeValue,
+    move1: move1Value,
+    move2: move2Value,
+    imageurl: imageurl,
   };
 }
 
-function injectObject(object) {
-  const CardHTML = `<div class="card">
-    <img src="${object.imageurls}" alt="Image/Error">
-    <p>Name: ${object.names}</p>
-    <p>Type: ${object.types}</p>
-    <p>Moves: ${object.moves}</p>
+function injectobject(object) {
+  const cardHTML = `<div class="card">
+    <img src="${object.imageurl}" alt="Image/Error">
+    <p>Name:${object.name}</p>
+    <p>Type:${object.type}</p>
+    <p>Move1${object.move1}</p>
+    <p>Move2${object.move2}</p>
 
-    <button class="remove">Remove</button>
+    <button class="removebutton">Remove</button>
     </div>`;
-  
-  DOMSelectors.container.insertAdjacentHTML("beforeend", CardHTML);
+    DOMSelectors.container.insertAdjacentHTML("beforeend", cardHTML);
 }
 
-function ClearInputFields() {
-  document.querySelector(".form").reset();
+function ClearInputFields() { 
+  DOMSelectors.image.value = "";
+  DOMSelectors.name.value = "";
+  DOMSelectors.type.value = "";
+  DOMSelectors.move1.value = "";
+  DOMSelectors.move2.value = "";
+
 }
 
-function removeObject() {
-  CardHTML.addEventListener("submit", function (event) {
-    this.CardHTML.remove();
-  });
-  
+function removeCard(event) {
+  if (event.target.classList.contains("removebutton")) {
+    const card = event.target.closest(".card");
+    card.remove();
+  }
 }
+
+DOMSelectors.container.addEventListener("click", removeCard);
+
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const newobject = createObject();
-  injectObject(newobject);
+  const newobject = createobject();
+  injectobject(newobject);
   ClearInputFields();
 });
+
 
 /*
     console.log(DOMSelectors.name.value);
